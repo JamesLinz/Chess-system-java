4b82a0bd6ffc62e057f5967f4a2899eb995fd827
JamesLinz <jlalencar@gmail.com>
2021-08-06 17:31:13 -0300

    Tratando exceções e limpeza de tela

--- src/application/UI.java
+++ src/application/UI.java
@@ -7,15 +7,20 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 
 public class UI {
-	
+
+	  // https://stackoverflow.com/questions/2979383/java-clear-the-console
+	  public static void clearScreen() { 
+		  
+		  System.out.flush();
+	  }
+
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
-		String s = sc.nextLine();
-		char column = s.charAt(0);
-		int row = Integer.parseInt(s.substring(1));
-		return new ChessPosition(column, row);
-	    }
-		catch (RuntimeException e) {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		} catch (RuntimeException e) {
 			throw new InputMismatchException("Erro lendo posição de xadrez. Valores válidos são de a1 até h8");
 		}
 	}
@@ -34,8 +39,7 @@ public class UI {
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
 			System.out.print("-");
-		}
-		else {
+		} else {
 			System.out.print(piece);
 		}
 		System.out.print(" ");
