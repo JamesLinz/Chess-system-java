c3d11f02f0d69d5df74491276bb6b60dde5b2acf
JamesLinz <jlalencar@gmail.com>
2021-08-10 19:39:31 -0300

    Melhorias na jogada Promoção

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,11 +1,8 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import com.sun.jdi.Type;
 
 import boardgame.Board;
 import boardgame.Piece;
@@ -125,7 +122,7 @@ public class ChessMatch {
 			throw new IllegalStateException("Não há peça para ser promovida");
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
-			throw new InvalidParameterException("Inválido para promoção");
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
